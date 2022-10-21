d69db70ec1fd6ae9881d871ddfa4e556d5220678
Megan <megantech@hotmail.com>
2022-10-21 21:11:51 +0000

    add default home page route in views & logout route in auth

--- openroad/auth.py
+++ openroad/auth.py
@@ -23,3 +23,9 @@ def login():
             flash('Email does not exist.', category='error')
 
     return render_template("login.html", user=current_user)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("views.home"))
--- openroad/views.py
+++ openroad/views.py
@@ -4,3 +4,8 @@ from flask_login import login_required, current_user
 
 
 views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    return render_template("home.html", user=current_user)
+
