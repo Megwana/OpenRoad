8e5cbbfadb7be6a3be23227f7773dfc60dc3fe7a
Megan <megantech@hotmail.com>
2022-10-21 21:18:32 +0000

    add signup route with flash messages stating minimum character requirements

--- openroad/auth.py
+++ openroad/auth.py
@@ -29,3 +29,32 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("views.home"))
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('first_name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User().query.filter_by(email=email).first()
+        if user:
+            flash('This Email already exists', category='error')
+        elif len(email) < 4:
+            flash('Your Email must be greater than 4 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('Your First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match, please try again.', category='error')
+        elif len(password1) < 8:
+            flash('Password must be a minimum of 8 characters.', category='error')
+        else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created!', category='success')
+            login_user(new_user, remember=True)
+            return redirect(url_for('views.home'))
+
+    return render_template("signup.html", user=current_user)
