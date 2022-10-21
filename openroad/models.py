b352f1a2a56586bdf7afdd59d1ec2a4f9731de07
Megan <megantech@hotmail.com>
2022-10-21 21:39:28 +0000

    add class road trip

--- openroad/models.py
+++ openroad/models.py
@@ -1,3 +1,4 @@
+from openroad import db
 from flask_login import UserMixin
 
 
@@ -6,3 +7,16 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(250))
     first_name = db.Column(db.String(50))
+    class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(250))
+    first_name = db.Column(db.String(50))
+    notes = db.relationship('Roadtrip')
+
+
+class Roadtrip(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(5000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
