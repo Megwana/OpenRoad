930cc489124a5a415c56af554c3c8413481af96e
Megan <megantech@hotmail.com>
2022-10-21 21:28:09 +0000

    import user from models and key login manager code.Add def for database (db)

--- openroad/__init__.py
+++ openroad/__init__.py
@@ -13,3 +13,17 @@ from .auth import auth
 
 app.register_blueprint(views, url_prefix='/')
 app.register_blueprint(auth, url_prefix='/')
+
+from openroad.models import User
+
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+def init_db():
+    with app.app_context():
+        db.create_all()
