c12f86fba8e85fc03d320a3bcba236e13487c2e5
Megan <megantech@hotmail.com>
2022-10-21 21:24:24 +0000

    Import views and auth info to init and app blueprint

--- openroad/__init__.py
+++ openroad/__init__.py
@@ -7,3 +7,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "helloworld"
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///openroad"
 db = SQLAlchemy(app)
+
+from .views import views
+from .auth import auth
+
+app.register_blueprint(views, url_prefix='/')
+app.register_blueprint(auth, url_prefix='/')
