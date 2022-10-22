cf0a2b6e7a5352b162ebceccb4b9a7dd68de09ef
Megan <megantech@hotmail.com>
2022-10-22 16:19:24 +0000

    add Roadtrip class now connected in plan.html to add trip notes

--- openroad/views.py
+++ openroad/views.py
@@ -1,7 +1,8 @@
 """import blueprint from flask"""
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
-
+from .models import Roadtrip
+from openroad import db
 
 views = Blueprint('views', __name__)
 
@@ -21,6 +22,16 @@ def trips():
 def previewtrips():
     return render_template("preview.html", user=current_user)
 
-@views.route('/plan')
+@views.route('/plan', methods=['GET', 'POST'])
 def plans():
+    if request.method == 'POST':
+        plan = request.form.get('plan')
+        if len(plan) <= 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_plan = Roadtrip(data=plan, user_id=current_user.id)
+            db.session.add(new_plan)
+            db.session.commit()
+            flash('Trip note added!', category='success')
+
     return render_template("plan.html", user=current_user)
